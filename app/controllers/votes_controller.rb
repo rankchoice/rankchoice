ad7d95ba02fcf4ff7a57320cc5ee8dc953fcd9a1
Cyrus Stoller <cyrus.stoller@gmail.com>
2019-02-03 11:01:59 -0800

    Redirect back to the election after casting a vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -30,7 +30,7 @@ class VotesController < ApplicationController
 
     respond_to do |format|
       if @vote.save
-        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
+        format.html { redirect_to @vote.election || root_path, notice: 'Vote was successfully created.' }
         format.json { render :show, status: :created, location: @vote }
       else
         format.html { render :new }
